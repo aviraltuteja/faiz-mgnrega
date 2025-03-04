573174e413afcb5d80c9044d6a3ef0433882be2f
aviraltuteja <aviraltuteja@gmail.com>
2025-03-04 05:43:08 +0530

    rerouted to iWork

--- iApplications/app/__init__.py
+++ iApplications/app/__init__.py
@@ -1,12 +1,15 @@
-# app.py
+# # app.py
 
-from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
+# from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
 
-from iCore import app as Core
+# from iCore import app as Core
+# from iWork import app as Work
+
+# application = DispatcherMiddleware(Core, {
+#     '/iwork':Work
+# })
+
+# # application = iCore
 from iWork import app as Work
 
-application = DispatcherMiddleware(Core, {
-    '/iwork':Work
-})
-
-# application = iCore
+application = Work
